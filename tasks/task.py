0f84a993a3cd9e1f35677f1c4ef9db15b44a85a3
Gruzin <gruzd.roman@inbox.ru>
2023-04-18 15:57:37 +0300

    Commit

--- tasks/task.py
+++ tasks/task.py
@@ -1,28 +1,31 @@
 from abc import ABC
-# from abc import abstractmethod
+from abc import abstractmethod
 
 
 class Vehicle(ABC):
-    def __init__(
-            self,
-            brand_name: str,
-            year_of_issue: int,
-            base_price: int,
-            mileage: int
-    ):
-        pass
 
+    def __init__(self, brand_name: str, year_of_issue: int, base_price: int, mileage: int):
+        self.brand_name = brand_name
+        self.year_of_issue = year_of_issue
+        self.base_price = base_price
+        self.mileage = mileage
+
+    @abstractmethod
     def wheels_num(self) -> int:
         return 0
 
     def vehicle_type(self) -> str:
-        pass
+        string = str(self.brand_name) + ' ' + str(self.__class__.__name__)
+        return string
 
     def is_motorcycle(self) -> bool:
-        pass
+        return isinstance(self, Motorcycle) == True
 
     def purchase_price(self) -> float:
-        pass
+        if self.base_price < 100_000:
+            return float(100_000)
+        else:
+            return self.base_price - 0.1 * self.mileage
 
 
 # Don't change class implementation
@@ -47,3 +50,4 @@ class Truck(Vehicle):
 class Bus(Vehicle):
     def wheels_num(self):
         return 6
+
